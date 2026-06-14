519c857ddfbfc50cc0c550880cd6ceb3369d128e
nairx <nairxprv@gmail.com>
2026-06-14 21:09:24 +0530

    updated

---
+++ mysql-notes/q1.sql
@@ -0,0 +1,19 @@
+system cls;
+
+create database mydb;
+
+show database;
+
+use mydb - to switch to mydb
+
+create table
+    employees (
+        id int,
+        name varchar(100),
+        email varchar(200),
+        department varchar(50),
+        salary int
+    );
+
+show tables;
+
---
+++ mysql-notes/q2.sql
@@ -0,0 +1,7 @@
+insert into
+    employees (id, name, email, department, salary)
+values
+    (1, "John", "john@gmail.com", "IT", 3500);
+
+
+select * from employees;
