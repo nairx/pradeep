e3272139eab53a479c95c2311fe48e3943884c8f
nairx <nairxprv@gmail.com>
2026-08-28 20:47:24 +0530

    update

---
+++ python-lessons/part1/p15.py
@@ -0,0 +1,20 @@
+# i=1
+# while i<5:
+#     print(i)
+#     i=i+1
+# print("Program Completed")
+
+
+# i=5
+# while i<36:
+#     print(i)
+#     i=i+5
+# print("Program Completed")
+
+
+i=10
+while i>0:
+    print(i)
+    i=i-1
+print("Program Completed")
+
